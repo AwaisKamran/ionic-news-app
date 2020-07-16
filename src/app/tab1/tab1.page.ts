b179c401bc88f77b0809e67f60631073a05d0383
Awais Kamran <awais.kamran@hotmail.com>
2020-07-16 11:31:58 +0500

    Ionic app: feat(News app) files added

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import NewsAPI from 'newsapi/dist/index.js';
+import { StorageService } from '../storage.service';
+const newsapi = new NewsAPI('cea50ef689684c3faf641ad2ad03da30');
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,36 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public news: any[] = [];
 
+  public newsList: any[] = [];
+
+  public value: string = "";
+
+  constructor(public storageService: StorageService) {
+    newsapi.v2.topHeadlines({
+      category: 'business',
+      language: 'en',
+      country: 'us'
+    }).then(response => {
+      this.news = response.articles;
+      this.newsList = [...this.news];
+
+      this.newsList.map((item)=>{
+        this.storageService.get(item.url).then(result => {
+          item.like = result;
+        });
+      }); 
+    });
+  }
+
+  filterNews(event) {
+    let value = event.target.value.toLowerCase();
+    this.newsList = value !== undefined || value !== "" ? this.news.filter((item) => item.title.toLowerCase().includes(value)) : [...this.news];
+  }
+
+  reactToPost(value, url, index) {
+    this.newsList[index].like = value;
+    this.storageService.set(`${url}`, value);
+  }
 }
---
+++ src/app/tab4/tab4.page.ts
@@ -0,0 +1,29 @@
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab4',
+  templateUrl: 'tab4.page.html',
+  styleUrls: ['tab4.page.scss']
+})
+export class Tab4Page {
+
+  public config: CameraOptions;
+
+  constructor(private camera: Camera) {
+    this.config = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(this.config).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+     }, (err) => {
+      // Handle error
+     });
+  }
+}
